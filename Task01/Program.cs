3e443eb8399f3b2f073a93c2c3a6d8a278a02689
Ivan <iss04@yandex.ru>
2023-04-09 01:05:57 +0300

    Wrote code for Task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+// a = 25, b = 5 -> да
+// a = 2, b = 10 -> нет
+// a = 9, b = -3 -> да
+// a = -3 b = 9 -> нет
+
+//мысли: итак, введены два числа. Если число1 поделить на число2 равно число1 то это да,
+//является квадратом. А если нет, то нет.
+
+Console.WriteLine("Введите первое число: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ваши числа {numberA} и {numberB}");
+
+if (numberA== numberB*numberB){
+    Console.WriteLine($"Да, число {numberA} является квадратом {numberB}");
+}
+else{
+    Console.WriteLine($"Нет, число {numberA} не является квадратом {numberB}");
+}
