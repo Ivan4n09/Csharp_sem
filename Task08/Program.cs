21e8ab30a82e17d624578063f36f56e4fd5fe66d
Ivan <iss04@yandex.ru>
2023-04-09 03:34:28 +0300

    Исправил Task08

--- Task08/Program.cs
+++ Task08/Program.cs
@@ -3,31 +3,15 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-// не мой код, более красивый, но я не усвоил новое в нем
-
-// Console.WriteLine("Введите положительное натуральное число: ");
-// if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
-// {
-//     int[] numbers = Enumerable.Range(1, number).ToArray();
-//     Console.WriteLine(string.Join(", ", numbers));
-// }
-// else
-// {
-//     Console.WriteLine("Вы ввели неправильное число");
-// }
-
-// а далее мой
-
 Console.WriteLine("Введите положительное натуральное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int count = 1;
+int count = 0;
 if (number > 0)
 while (count<=number)
 {
     Console.Write(count+", ");
-    count++;
-    // count = count + 1
-    // count += 1
+    count += 2;
+    // count = count + 2
 }
 else
     Console.WriteLine("Вы ввели неправильное число");
